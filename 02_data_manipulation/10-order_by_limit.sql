48bcd17cfba0837493fd3c552fbd87e20b1e0137
kayz <robinburri@tutanota.com>
2023-03-01 19:09:15 +0100

    add DISTINCT to 10

--- 02_data_manipulation/10-order_by_limit.sql
+++ 02_data_manipulation/10-order_by_limit.sql
@@ -2,7 +2,7 @@
 -- Drop any duplicates in the result set
 
 -- Find top 10 sales
--- SELECT * FROM sales ORDER BY volume DESC LIMIT 10;
+-- SELECT DISTINCT * FROM sales ORDER BY volume DESC LIMIT 10;
 
 -- Find bottom 10 sales
-SELECT * FROM sales ORDER BY volume ASC LIMIT 10;
+SELECT DISTINCT * FROM sales ORDER BY volume ASC LIMIT 10;
