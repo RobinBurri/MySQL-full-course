5c8094b70ce5c49fd967e2e84020924a038c0212
kayz <robinburri@tutanota.com>
2023-03-06 19:57:28 +0100

    data-relationship started

---
+++ data_relationship/02-insert-data.sql
@@ -0,0 +1,15 @@
+-- INSERT INTO cities (name)
+-- VALUES ('Lausanne'),
+--     ('London'),
+--     ('New York'),
+--     ('Berlin');
+-- INSERT INTO addresses (street, house_number, city_id)
+-- VALUES ('Teststreet', 'A8', 2),
+--     ('totostreet', '10', 4),
+--     ('Teststreet', '1', 2),
+--     ('Mystreet', '101', 1);
+INSERT INTO users (first_name, last_name, email, address_id)
+VALUES ('Robin', 'Burri', 'robinburri@test.com', 1),
+    ('Max', 'Schwartz', 'maxschwartz@test.com', 4),
+    ('John', 'Doe', 'johndoe@test.com', 2),
+('Jane', 'Eod', 'janeeod@test.com', 2);
