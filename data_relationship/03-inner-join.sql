ae5fdfbcab697ee8f9b7f566ca2e0b0273d6f3ae
kayz <robinburri@tutanota.com>
2023-05-19 21:47:45 +0200

    double inner join

--- data_relationship/03-inner-join.sql
+++ data_relationship/03-inner-join.sql
@@ -1,5 +1,9 @@
-SELECT u.first_name,
-    a.street,
-    a.house_number
+SELECT u.id,
+    first_name,
+    last_name,
+    street,
+    house_number,
+    c.name
 FROM users AS u
     INNER JOIN addresses AS a ON u.address_id = a.id
+    INNER JOIN cities AS c ON a.city_id = c.id
