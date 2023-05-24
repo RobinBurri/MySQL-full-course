e4f0625eae6c83f125645d07accb91f8aef08822
Robin Burri <robinburri@tutanota.com>
2023-05-24 15:40:33 +0200

    data_relation_exemple created

---
+++ data_relation_exemple.sql/02-insert-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO employee (first_name, last_name, birthdate, email)
+VALUES ('Robin', 'Burri', '1989-06-10', 'test@test.com');
+
+
+INSERT INTO intranet_accouts (email, password)
+VALUES('test@test.com', 'abcde1');
